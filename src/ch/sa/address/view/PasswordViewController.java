e488164dd7c55a7102b2da04fbbf56aa4bcfcf28
ViralVince <vincent.peuker@gmail.com>
2018-02-23 11:07:44 +0100

    V1

---
+++ src/ch/sa/address/view/PasswordViewController.java
@@ -0,0 +1,56 @@
+package ch.sa.address.view;
+
+
+import ch.sa.address.MainApp;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TextField;
+
+public class PasswordViewController {
+
+	@FXML
+    private TableColumn<String ,String> loginColumn;
+
+	@FXML
+	private TextField passwordField;
+
+	@FXML
+	private Button RevealButton;
+
+	@FXML
+	private Button CopyButton;
+
+	@FXML
+	private Button EnlargeButton;
+
+	private MainApp mainApp;
+
+	public PasswordViewController() {
+    }
+	@FXML
+	public void setupPasswordViewScene(){
+
+
+	}
+
+	@FXML
+    private void initialize() {
+		passwordField.setText("********");
+
+    }
+
+	public void handleRevealPassword(){
+		if(RevealButton.getText().equals("Reveal")){
+		passwordField.setText("Passwort");
+		RevealButton.setText("Hide");
+		} else {
+			passwordField.setText("********");
+			RevealButton.setText("Reveal");
+		}
+	}
+	public void editScene(){
+
+    }
+
+}
