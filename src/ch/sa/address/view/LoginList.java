166598655ce0c551e74c46f258b5beac6ba5f753
ViralVince <vincent.peuker@gmail.com>
2018-03-31 13:04:20 +0200

    gg

--- src/ch/sa/address/view/LoginList.java
+++ src/ch/sa/address/view/LoginList.java
@@ -1,6 +1,11 @@
 package ch.sa.address.view;
 
+import java.io.File;
 import java.util.ArrayList;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
 
 public class LoginList {
 
@@ -10,8 +15,20 @@ public class LoginList {
 		ar.add(l);
 	}
 
-	public void createXML(){
+	public void createXML(Login l){
+		try{
+		File file = new File("/data.xml");
+		JAXBContext jaxbContext = JAXBContext.newInstance(Login.class);
+		Marshaller jaxbMarshaller = jaxbContext.createMarshaller();
 
+		// output pretty printed
+		jaxbMarshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
+
+		jaxbMarshaller.marshal(l, file);
+		jaxbMarshaller.marshal(l, System.out);
+		} catch (JAXBException e) {
+			e.printStackTrace();
+		      }
 	}
 
 }
